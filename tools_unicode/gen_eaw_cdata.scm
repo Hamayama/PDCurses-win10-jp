fef5dd9acd31f9cae9d6c21d593b3da6bd829ac1
Hamayama <hamay1010@gmail.com>
2020-12-08 18:07:05 +0900

    unicode tool chg

--- tools_unicode/gen_eaw_cdata.scm
+++ tools_unicode/gen_eaw_cdata.scm
@@ -1,7 +1,7 @@
 ;; -*- coding: utf-8 -*-
 ;;
 ;; gen_eaw_cdata.scm
-;; 2020-12-8 v1.02
+;; 2020-12-8 v1.03
 ;;
 ;; ＜内容＞
 ;;   Gauche を使用して、C言語用の文字幅データを生成するためのツールです。
@@ -63,24 +63,19 @@
 ;;   ・範囲のリストは、開始点でソート済みであること
 (define (compress-range-list range-list)
   (define result-range-list '())
-  (define r-last #f)
   (for-each
    (lambda (r-now)
      ;; 条件を満たせば、最後の範囲にマージする
-     (cond
-      ((and r-last
-            (<= (range-start r-now)  (+ (range-end r-last) 1))
-            (<= (range-start r-last) (+ (range-end r-now)  1)))
-       ;; マージ処理
-       (set! (range-start r-last) (min (range-start r-last)
-                                       (range-start r-now)))
-       (set! (range-end   r-last) (max (range-end r-last)
-                                       (range-end r-now))))
-      (else
-       ;; r-last を変更可能(mutable)とするため、copy-range によるコピーが必要
-       (set! r-last (copy-range r-now))
-       (push! result-range-list r-last)))
-     )
+     (let ((r-last (if (null? result-range-list)
+                     #f
+                     (car result-range-list))))
+       (if (and r-last
+                (<= (range-start r-now)  (+ (range-end r-last) 1))
+                (<= (range-start r-last) (+ (range-end r-now)  1)))
+         (set-car! result-range-list
+                   (make-range (min (range-start r-now) (range-start r-last))
+                               (max (range-end   r-now) (range-end   r-last))))
+         (push! result-range-list r-now))))
    range-list)
   (reverse result-range-list))
 
@@ -90,13 +85,12 @@
 ;;   ・範囲のリスト1と2は、開始点でソート済みであること
 (define (merge-range-list range-list-1 range-list-2)
   (define result-range-list '())
-  (define r-now  #f)
-  (define r-last #f)
   (cond
    ((null? range-list-1) range-list-2)
    ((null? range-list-2) range-list-1)
    (else
-    (let loop ((r1    (car range-list-1))
+    (let loop ((r-now #f)
+               (r1    (car range-list-1))
                (r2    (car range-list-2))
                (rest1 range-list-1)
                (rest2 range-list-2))
@@ -111,23 +105,21 @@
         (set! r-now r2)
         (set! rest2 (cdr rest2))))
       ;; 条件を満たせば、最後の範囲にマージする
-      (cond
-       ((and r-last
-             (<= (range-start r-now)  (+ (range-end r-last) 1))
-             (<= (range-start r-last) (+ (range-end r-now)  1)))
-        ;; マージ処理
-        (set! (range-start r-last) (min (range-start r-last)
-                                        (range-start r-now)))
-        (set! (range-end   r-last) (max (range-end r-last)
-                                        (range-end r-now))))
-       (else
-        ;; r-last を変更可能(mutable)とするため、copy-range によるコピーが必要
-        (set! r-last (copy-range r-now))
-        (push! result-range-list r-last)))
+      (let ((r-last (if (null? result-range-list)
+                      #f
+                      (car result-range-list))))
+        (if (and r-last
+                 (<= (range-start r-now)  (+ (range-end r-last) 1))
+                 (<= (range-start r-last) (+ (range-end r-now)  1)))
+          (set-car! result-range-list
+                    (make-range (min (range-start r-now) (range-start r-last))
+                                (max (range-end   r-now) (range-end   r-last))))
+          (push! result-range-list r-now)))
       ;; リストの残りをチェック
       (if (and (null? rest1) (null? rest2))
         (reverse result-range-list)
-        (loop (if (null? rest1) #f (car rest1))
+        (loop #f
+              (if (null? rest1) #f (car rest1))
               (if (null? rest2) #f (car rest2))
               rest1
               rest2))))))
@@ -142,9 +134,9 @@
     (let ((start (string->number start 16))
           (end   (string->number end   16))
           (type  (string->symbol type)))
-      (if (and (memq type wide-type-selector)
-               ;(< start #xf0000) ; 一部範囲を限定
-               )
+      (when (and (memq type wide-type-selector)
+                 ;(< start #xf0000) ; 一部範囲を限定
+                 )
         (push! result-range-list (make-range start end)))))
   (for-each
    (lambda (line)
@@ -165,8 +157,8 @@
     (let ((start (string->number start 16))
           (end   (string->number end   16))
           (type  (string->symbol type)))
-      (if (and (eq? type 'Emoji)
-               (>= start #x1000)) ; 一部範囲を限定
+      (when (and (eq? type 'Emoji)
+                 (>= start #x1000)) ; 一部範囲を限定
         (push! result-range-list (make-range start end)))))
   (for-each
    (lambda (line)
